af19368a52da7b82ee33b6b3de48f0428cac53cd
Jace Manshadi <jaymanshad@proton.me>
2026-03-22 22:59:50 -0700

    still using runserver

--- .ci/entrypoint.sh
+++ .ci/entrypoint.sh
@@ -2,7 +2,8 @@
 
 # Wait for DB to be ready if needed, then migrate
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+
+# python manage.py collectstatic --noinput not needed yet
 
 # Start the actual application
 exec "$@"
