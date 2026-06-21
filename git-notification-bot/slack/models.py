80290b2a2148598686811a9d97f9aa1e98a90cee
Jace Manshadi <jaymanshad@proton.me>
2026-06-20 23:50:13 -0700

    more logging

--- git-notification-bot/slack/models.py
+++ git-notification-bot/slack/models.py
@@ -1,19 +1,5 @@
 from django.db import models
 
-from core.models import ClientConfig as core_config  # noqa: N813
-
-
-# Create your models here.
-
-class ClientConfig(models.Model):
-    slack_bot_user_oauth_token = models.CharField(
-        null=True,
-        blank=True
-    )
-    client = models.ForeignKey(
-        core_config, on_delete=models.CASCADE
-    )
-
 
 class SlackInstallation(models.Model):
     team_id = models.CharField(max_length=50, primary_key=True, help_text="Slack Workspace ID (starts with T)")
