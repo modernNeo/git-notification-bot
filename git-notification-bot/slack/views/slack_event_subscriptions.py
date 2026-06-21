ee5041c5145cc8788c18ee305d6cee5481a55b5b
Jace Manshadi <jaymanshad@proton.me>
2026-06-21 00:27:09 -0700

    prefixing the url

--- git-notification-bot/slack/views/slack_event_subscriptions.py
+++ git-notification-bot/slack/views/slack_event_subscriptions.py
@@ -1,9 +1,13 @@
+import json
+
 from django.http import HttpResponse
+from django.utils.decorators import method_decorator
 from django.views import View
-
 from core.LogRequestData import log_request_data
+from django.views.decorators.csrf import csrf_exempt
 
 
+@method_decorator(csrf_exempt, name="dispatch")  # 3. Apply the exemption
 class SlackEventSubscriptions(View):
     """Generates the secure state and redirects the user to Slack."""
 
@@ -13,6 +17,18 @@ class SlackEventSubscriptions(View):
 
     def post(self, request, *args, **kwargs):
         log_request_data(request)
+
+        # Parse Slack's JSON payload
+        try:
+            data = json.loads(request.body.decode("utf-8"))
+
+            # If Slack is testing the URL, return the challenge parameter
+            if data.get("type") == "url_verification":
+                return HttpResponse(data.get("challenge"), content_type="text/plain")
+
+        except json.JSONDecodeError:
+            pass
+
         return HttpResponse("POST SlackEventSubscriptions.")
 
     def patch(self, request, *args, **kwargs):
